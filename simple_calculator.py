7dec6c1d3cf2a39aac851d29c4d0dbafef8073db
Sanjay <sanjay8874002308@gmail.com>
2022-05-02 15:38:23 +0530

    added function

---
+++ simple_calculator.py
@@ -0,0 +1,17 @@
+# Calculator
+
+def add(a,b):
+    return a+b
+
+def substract(a,b):
+    return a-b
+
+def divide(a,b):
+    return a/b
+
+def multiplication(a,b):
+    return a*b
+print("Enter the choice 1:add, 2:substract, 3:divide, 4:multiplication")
+choice = input("Eneter the operation 1,2,3,4")
+    
+
